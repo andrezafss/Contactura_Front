8a0439a490d11d37755068c02bc15fc77320e056
andrezafss <dezaf.ferreira@gmail.com>
2021-07-08 19:38:34 -0300

    Criacao dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { ListaContatosComponent } from './lista-contatos/lista-contatos.componen
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 import { FormUsuariosComponent } from './form-usuarios/form-usuarios.component';
 import { FormContatosComponent } from './form-contatos/form-contatos.component';
+import { NotFoundComponent } from './sharedComponent/not-found/not-found.component';
+//imports do material
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
@@ -17,12 +24,17 @@ import { FormContatosComponent } from './form-contatos/form-contatos.component';
     ListaContatosComponent,
     ListaUsuariosComponent,
     FormUsuariosComponent,
-    FormContatosComponent
+    FormContatosComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
